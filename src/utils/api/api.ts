6836b8e3be7f429ded341e330412b8a55e30f661
Vo Thanh Luan <s3822042@rmit.edu.vn>
2022-07-18 15:28:58 +0700

    chore: add boierplace code for constant and api

---
+++ src/utils/api/api.ts
@@ -0,0 +1,7 @@
+const localHostFE = "http://localhost:3000/";
+
+const api = {
+  urlDomain: localHostFE, // localHostFE(dev) or cloudDomain(production)
+};
+
+export default api;
---
+++ src/utils/constants/image.constant.ts
@@ -0,0 +1,7 @@
+const banner = "http://www.example.com";
+
+const image = {
+  bannerImg: banner,
+};
+
+export default image;
