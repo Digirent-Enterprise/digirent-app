de33b2e91fab2a78eb96816390db4b6460e7d9ae
Vo Thanh Luan <s3822042@rmit.edu.vn>
2022-07-18 15:29:22 +0700

    feat: add global fonts and color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./src/*.{ts,tsx}", "./src/**/*.{ts,tsx}", "./public/index.html"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#222",
+      white: "#fff",
+      blue: {
+        100: "#4169E1",
+        200: "#1010AE",
+      },
+      red: "#AE1010",
+      green: "#11995B",
+    },
     extend: {},
   },
   plugins: [],
