d0d228fcb30493ce7f09f10fa54bf4432deedb08
Vo Thanh Luan <s3822042@rmit.edu.vn>
2022-07-25 18:02:53 +0700

    fix: css bug inline center

--- src/index.tsx
+++ src/index.tsx
@@ -26,51 +26,49 @@ root.render(
       <ChakraProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<App />}>
-              <Route path="login" />
-              <Route path="logout" />
-              <Route path="register" />
-              <Route path="reset-password" />
-              <Route path="send-email" />
-              <Route path="forgot-password" />
-              {/* Product */}
-              <Route path="products" element={<ProductSearchPage />} />
-              <Route path="products/?category=phone" />
-              <Route path="products/:id" />
-              {/* Payment */}
-              <Route path="checkout/:id" />
-              <Route path="checkout-success/:id" />
-              {/* Users */}
-              <Route path="users" />
-              <Route path="users/:id" />
-              <Route path="users/:id/profile" />
-              <Route path="users/:id/favorite-products" />
-              <Route path="users/:id/edit" />
-              <Route path="users/:id/deactivate" />
-              <Route path="users/:id/change-password" />
-              {/* Maintain */}
-              <Route path="maintain" element={<Maintain />} />
-            </Route>
+            <Route path="/" element={<App />} />
+            <Route path="login" />
+            <Route path="logout" />
+            <Route path="register" />
+            <Route path="reset-password" />
+            <Route path="send-email" />
+            <Route path="forgot-password" />
+            {/* Product */}
+            <Route path="/products" element={<ProductSearchPage />} />
+            <Route path="products/?category=phone" />
+            <Route path="products/:id" />
+            {/* Payment */}
+            <Route path="checkout/:id" />
+            <Route path="checkout-success/:id" />
+            {/* Users */}
+            <Route path="users" />
+            <Route path="users/:id" />
+            <Route path="users/:id/profile" />
+            <Route path="users/:id/favorite-products" />
+            <Route path="users/:id/edit" />
+            <Route path="users/:id/deactivate" />
+            <Route path="users/:id/change-password" />
+            {/* Maintain */}
+            <Route path="maintain" element={<Maintain />} />
             {/* Admin */}
-            <Route path="admin">
-              {/* User management */}
-              <Route path="users" element={<UserManagement />} />
-              <Route path="users/:id/edit" />
-              <Route path="users/:id/delete" />
-              {/* Product management */}
-              <Route path="products" element={<ProductManagement />} />
-              <Route path="add-product" />
-              <Route path="product/:id" />
-              <Route path="products/:id/edit" />
-              <Route path="products/:id/delete" />
-              {/* Transaction management */}
-              <Route path="transactions" element={<TransactionManagement />} />
-              <Route path="transactions/:id/edit" />
-              <Route path="transactions/:id/delete" />
-              {/* Chat */}
-              <Route path="chat" />
-              <Route path="chat/:id" />
-            </Route>
+            <Route path="admin" />
+            {/* User management */}
+            <Route path="users" element={<UserManagement />} />
+            <Route path="users/:id/edit" />
+            <Route path="users/:id/delete" />
+            {/* Product management */}
+            <Route path="products" element={<ProductManagement />} />
+            <Route path="add-product" />
+            <Route path="product/:id" />
+            <Route path="products/:id/edit" />
+            <Route path="products/:id/delete" />
+            {/* Transaction management */}
+            <Route path="transactions" element={<TransactionManagement />} />
+            <Route path="transactions/:id/edit" />
+            <Route path="transactions/:id/delete" />
+            {/* Chat */}
+            <Route path="chat" />
+            <Route path="chat/:id" />
 
             {/* 404 Not Found Route */}
             <Route path="*" element={<NotFound />} />
