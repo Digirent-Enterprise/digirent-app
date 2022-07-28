8324f67bed6ff9297db95690c42753622920d58b
s3822042 <s3822042@rmit.edu.vn>
2022-07-28 21:25:07 +0700

    feat: add google oauth support

--- src/components/form/AuthForm/AuthFormGrid.tsx
+++ src/components/form/AuthForm/AuthFormGrid.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
+
+import { getGoogleUrl } from "../../../utils";
 
 interface ParentCompProps {
-  childCompForm?: React.ReactNode;
-  childCompSideContent?: React.ReactNode;
-  childTitle?: React.ReactNode;
+  childCompForm?: ReactNode;
+  childCompSideContent?: ReactNode;
+  childTitle?: ReactNode;
 }
 
 const AuthFormGrid: React.FC<ParentCompProps> = (props) => {
@@ -11,18 +13,18 @@ const AuthFormGrid: React.FC<ParentCompProps> = (props) => {
 
   return (
     <div className="flex items-center min-h-screen bg-cover bg-auth">
-      <div className="flex-1 h-full max-w-4xl mx-auto bg-white opacity-90 rounded-lg shadow-xl">
+      <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl opacity-90">
         <div className="flex flex-col md:flex-row">
           <div className="h-32 md:h-auto md:w-1/2">
             <img
-              className="object-cover w-full h-full brightness-50 opacity-100"
+              className="object-cover w-full h-full opacity-100 brightness-50"
               src={`${childCompSideContent}`}
               alt="img"
             />
           </div>
           <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
             <div className="w-full">
-              <h1 className="italic mb-4 text-5xl font-bold text-center text-blue-100 p-5">
+              <h1 className="p-5 mb-4 text-5xl italic font-bold text-center text-blue-100">
                 Digirent
               </h1>
               <h1 className="mb-4 text-2xl font-bold text-center text-gray-700">
@@ -43,7 +45,7 @@ const AuthFormGrid: React.FC<ParentCompProps> = (props) => {
                   className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
                 >
                   <svg
-                    className="mr-2 -ml-1 w-4 h-4"
+                    className="w-4 h-4 mr-2 -ml-1"
                     aria-hidden="true"
                     focusable="false"
                     data-prefix="fab"
@@ -57,14 +59,14 @@ const AuthFormGrid: React.FC<ParentCompProps> = (props) => {
                       d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
                     />
                   </svg>
-                  Google
+                  <a href={getGoogleUrl()}>Google</a>
                 </button>
                 <button
                   type="button"
                   className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
                 >
                   <svg
-                    className="mr-2 -ml-1 w-4 h-4"
+                    className="w-4 h-4 mr-2 -ml-1"
                     aria-hidden="true"
                     focusable="false"
                     data-prefix="fab"
@@ -76,7 +78,7 @@ const AuthFormGrid: React.FC<ParentCompProps> = (props) => {
                     <path
                       fill="currentColor"
                       d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
-                    ></path>
+                    />
                   </svg>
                   Facebook
                 </button>
