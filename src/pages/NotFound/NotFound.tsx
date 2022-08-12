e85d62bd0db8df854b76a7555346f6ec9602363f
s3822042 <s3822042@rmit.edu.vn>
2022-08-12 15:46:25 +0700

    feat: change 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,60 +4,37 @@ const NotFound = () => {
   return (
     <>
       <Helmet title="Not Found" addPostfixTitle description="Not Found Page" />
-      <div className="py-6 bg-white sm:py-8 lg:py-12">
-        <div className="max-w-screen-lg px-4 mx-auto md:px-8">
-          <div className="grid gap-8 sm:grid-cols-2">
-            <div className="overflow-hidden bg-gray-100 rounded-lg shadow-lg h-80 md:h-auto">
-              <img
-                src="https://images.unsplash.com/photo-1626790680787-de5e9a07bcf2?auto=format&q=75&fit=crop&w=600"
-                loading="lazy"
-                alt="banner"
-                className="object-cover object-center w-full h-full"
-              />
+      <div className="min-h-full px-4 py-16 bg-white sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="mx-auto max-w-max">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-[#4F46E5] sm:text-5xl">
+              404
+            </p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-[#E5E7EB] sm:pl-6">
+                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+                  Page not found
+                </h1>
+                <p className="mt-1 text-base text-[#6B7280]">
+                  Please check the URL in the address bar and try again.
+                </p>
+              </div>
+              <div className="flex mt-10 space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <a
+                  href="/"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-[#4F46E5] border border-transparent rounded-md shadow-sm hover:bg-[#4338CA] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6366F1]"
+                >
+                  Go back home
+                </a>
+                <a
+                  href="/contact"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-[#4338CA] bg-[#E0E7FF] border border-transparent rounded-md hover:bg-[#C7D2FE] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6366F1]"
+                >
+                  Contact support
+                </a>
+              </div>
             </div>
-
-            <div className="flex flex-col items-center justify-center sm:items-start md:py-24 lg:py-32">
-              <p className="mb-4 text-sm font-semibold text-indigo-500 uppercase md:text-base">
-                Error 404
-              </p>
-              <h1 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl sm:text-left">
-                Page not found
-              </h1>
-
-              <p className="mb-4 text-center text-gray-500 md:text-lg sm:text-left md:mb-8">
-                The page you’re looking for doesn’t exist.
-              </p>
-
-              <nav className="flex gap-4 sm:block sm:space-y-1 md:space-y-2">
-                <div>
-                  <a
-                    href="/"
-                    className="inline-block text-sm text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700 md:text-base"
-                  >
-                    Home
-                  </a>
-                </div>
-
-                <div>
-                  <a
-                    href="/products"
-                    className="inline-block text-sm text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700 md:text-base"
-                  >
-                    Search
-                  </a>
-                </div>
-
-                <div>
-                  <a
-                    href="/contact"
-                    className="inline-block text-sm text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700 md:text-base"
-                  >
-                    Help
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </div>
+          </main>
         </div>
       </div>
     </>
