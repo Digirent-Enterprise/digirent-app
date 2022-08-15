83200b6a71bb2e38ee59517acad21e40257c4289
Vo Thanh Luan <62096559+s3822042@users.noreply.github.com>
2022-08-15 16:05:09 +0700

    Luan/admin dashboard (#64)
    * feat: switch to react-table
    
    * feat: code WIP
    
    * feat: fix mino changes
    
    * feat: change to permission for role-based configuration
    
    * feat: minor changes
    
    * hotfix: for product table
    
    * fix: selector now can render data
    
    * feat: add logout and add directUrl to navbar
    
    * fix: minor changes
    
    * feat: change image path
    
    * fix: default avatar in avatar menu ( absolute path )
    
    * feat: change constant file name for permission
    
    * feat: change fie constant name
    
    * fix: change file name , delete unneccesary file and fix some css style
    
    * feat: add new package
    
    * feat: add new eslint rule
    
    * feat: extract to smaller components for table and add favorites redux ( not fix bug yet )
    
    * feat: fix headers for react-csv
    
    * fix: minor changes
    
    * feat: add transaction columns and redux call saga
    
    * fix: transaction bug in action
    
    * feat: add transaction page for routing
    
    * feat: remove redundant export components
    
    * feat: add email sent page
    
    * feat: add CTA component
    
    * feat: code WIP
    
    * fix: minor changes
    
    * feat: add react lazy load for routing and suspense fallback spinner
    
    * feat: minor changes
    
    * feat: code WIP
    
    * feat: code WIP
    
    * feat: dynamically change meta tag and title depend on current route
    
    * feat: add components for landing page
    
    * feat: back to top indicator
    
    * feat: add custom scrollbar
    
    * feat: add helper constant
    
    * feat: minor changes related to design
    
    * feat: fix css
    
    * fix: minor changes
    
    * feat: change 404 page
    
    * feat: change to new logo and favicon
    
    * fix: minor changes
    
    * feat: add fetching product and finish landing page
    
    * feat: add multiple langue support
    
    * fix: minor changes
    
    * fix: prettier
    
    * hotfix: prettier and eslint
    
    * fix: remove unused css
    
    * fix: prettier
    
    * feat: add service worker and pwa
    
    * feat: analyzing bundle size
    
    * hotfix: change to use createSelector
    
    * fix: change to fragment for empty div
    
    * feat: change folder name
    
    * feat: delete unused file
    
    * fix: minor changes
    
    * fix: contact page
    
    * fix: ui for contact page
    
    * feat: add products components
    
    * feat: minor changes
    
    * feat: code WIP
    
    * fix: minor changes
    
    * fix: for teans member section
    
    * feat: finish banner

--- src/utils/constants/image.constant.ts
+++ src/utils/constants/image.constant.ts
@@ -1,4 +1,5 @@
-const phoneBannerImg = "/images/banner/phone-banner.png";
+const phoneBannerImg =
+  "https://discloud-storage.herokuapp.com/file/ee22f84e45c430e3affb08481234cee7/phone-banner.png";
 const defaultAvatar = "/images/default-avatar.svg";
 
 export const IMAGES = {
