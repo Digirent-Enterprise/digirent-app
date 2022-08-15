ea1366b3e48a22987ba3c9b69903160caf7c4f17
s3822042 <s3822042@rmit.edu.vn>
2022-08-15 14:36:14 +0700

    fix: minor changes

--- src/components/elements/Banner/Banner.tsx
+++ src/components/elements/Banner/Banner.tsx
@@ -30,11 +30,6 @@ const Banner = () => {
       </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
     </Swiper>
   );
 };
