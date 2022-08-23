4b1589f9ee2943fc40921475ff7c73acdeb0bf92
Vo Thanh Luan <62096559+s3822042@users.noreply.github.com>
2022-08-23 23:01:51 +0700

    Minh/change password (#95)
    * Change user profile UI like figma design
    
    * Add user change password page
    
    * feat: add change user avatar on hover
    
    * feat: add model for user avatar
    
    Co-authored-by: minh0811 <vokhaiminh0811@gmail.com>

--- src/components/elements/User/UserProfileAvatar.tsx
+++ src/components/elements/User/UserProfileAvatar.tsx
@@ -1,18 +1,96 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Avatar } from "@chakra-ui/react";
+import { useSelector, useDispatch } from "react-redux";
+import { Avatar, Text } from "@chakra-ui/react";
 import { getCurrentUserSelector } from "../../../store/selectors/user.selector";
 import { IMAGES } from "../../../utils/constants/image.constant";
+import AiOutlineCamera from "react-icons/ai";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import { useDropzone } from "react-dropzone";
+
+import { FormControl, FormLabel, GridItem } from "@chakra-ui/react";
 
 const UserProfileAvatar = () => {
   const currentUser = useSelector(getCurrentUserSelector);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: {
+      "image/*": [],
+    },
+    maxFiles: 10,
+    onDrop: async (acceptedFiles: File[]) => {
+      setIsLoading(true);
+      await acceptedFiles.map((file: any) => handleUploadFiles(file));
+      setIsLoading(false);
+      setImages(
+        acceptedFiles.map((image: any) =>
+          Object.assign(image, {
+            preview: image,
+          })
+        )
+      );
+    },
+  });
   return (
     <Avatar
       boxSize="350px"
       src={currentUser.avatar ? currentUser.avatar : IMAGES.defaultAvatar}
       mb={4}
       pos="relative"
-    />
+      className="overflow-hidden"
+    >
+      <div className="absolute bottom-0 ">
+        <Button
+          onClick={onOpen}
+          size="lg"
+          height="100px"
+          width="400px"
+          colorScheme="blackAlpha"
+          className="opacity-0 hover:opacity-100 hover:bg-[#222] absolute bottom-0 outline-none box-border cursor-pointer duration-500 z-30"
+        >
+          Change Avatar
+        </Button>
+      </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <GridItem className="pb-5 p-5" colSpan={{ base: 6, sm: 3 }}>
+            <FormControl>
+              <FormLabel>Add your avatar</FormLabel>
+              <div className="border-dashed border-4 text-center justify-center p-[20%]">
+                <input />
+                {isDragActive ? (
+                  <p>Drop the files here ...</p>
+                ) : (
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                )}
+              </div>
+            </FormControl>
+            <aside className="flex flex-row"></aside>
+          </GridItem>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button type="reset" mr={3}>
+              Reset
+            </Button>
+            <Button type="submit" mr={3}>
+              Change
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Avatar>
   );
 };
 
---
+++ src/pages/User/UserChangePassword.tsx
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  useColorModeValue,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { WarningTwoIcon } from "@chakra-ui/icons";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import qs from "qs";
+import { toast } from "react-toastify";
+import { UserTab } from "../../components";
+import DefaultLayout from "../DefaultLayout";
+import { getCurrentUserSelector } from "../../store/selectors/user.selector";
+import { customAxios } from "../../http-common";
+
+interface IFormInputs {
+  password: string;
+  changePassword: string;
+  retypeChangePassword: string;
+}
+
+const schema = yup.object().shape({
+  password: yup.string().required("Please enter your password!"),
+  changePassword: yup
+    .string()
+    .min(8, "Your password must be at least 8 characters!")
+    .max(15)
+    .required("Please enter your new password!"),
+  retypeChangePassword: yup
+    .string()
+    .oneOf([yup.ref("changePassword"), null], "Your passwords do not match!")
+    .required("Please retype your password!"),
+});
+
+const UserChangePassword = () => {
+  const currentUser = useSelector(getCurrentUserSelector);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+  });
+
+  const navigate = useNavigate();
+
+  const onCancel = () => {
+    navigate("/user/my-profile");
+  };
+
+  const onSubmit = async (data: IFormInputs) => {
+    const update = await customAxios().put(
+      "user/edit-user",
+      qs.stringify(data)
+    );
+    if (update.data) {
+      toast.success("Update user successfully", { theme: "dark", icon: "🚀" });
+    }
+  };
+
+  return (
+    <DefaultLayout>
+      <UserTab />
+      <Flex minH="70vh" align="center" justify="center" bg="#fff">
+        <Box
+          w="full"
+          maxW="700px"
+          bg={useColorModeValue("#fff", "#777")}
+          rounded="xl"
+          p={6}
+          my={12}
+        >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Link to="/user/my-profile">
+              <div className="flex">
+                <AiOutlineArrowLeft color="#4169E1" className="mx-2 text-3xl" />
+                <Text color="#4169E1" className="mx-2 mb-10 text-lg">
+                  Return to profile info
+                </Text>
+              </div>
+            </Link>
+
+            <FormControl isInvalid={!!errors?.password?.message} py="10px">
+              <FormLabel>Enter your current password:</FormLabel>
+              <Input
+                {...register("password")}
+                placeholder="Current password"
+                _placeholder={{ color: "#777" }}
+                type="text"
+                bg={useColorModeValue("gray.50", "gray.500")}
+                width="650px"
+              />
+              <FormErrorMessage>
+                <WarningTwoIcon /> {errors?.password?.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl
+              isInvalid={!!errors?.changePassword?.message}
+              py="10px"
+            >
+              <FormLabel>Enter your new password:</FormLabel>
+              <Input
+                {...register("changePassword")}
+                placeholder="New"
+                _placeholder={{ color: "#777" }}
+                type="text"
+                bg={useColorModeValue("gray.50", "gray.500")}
+                width="650px"
+              />
+              <FormErrorMessage>
+                <WarningTwoIcon /> {errors?.changePassword?.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl
+              isInvalid={!!errors?.retypeChangePassword?.message}
+              py="10px"
+            >
+              <FormLabel>Retype your new password:</FormLabel>
+              <Input
+                {...register("retypeChangePassword")}
+                placeholder="Retype your password"
+                _placeholder={{ color: "#777" }}
+                type="text"
+                bg={useColorModeValue("gray.50", "gray.500")}
+                width="650px"
+              />
+              <FormErrorMessage>
+                <WarningTwoIcon /> {errors?.retypeChangePassword?.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <Stack
+              spacing={6}
+              direction={["column", "row"]}
+              py="20px"
+              className="flex"
+            >
+              <Button
+                bg="grey"
+                color="white"
+                w="50%"
+                _hover={{
+                  bg: "#153289",
+                }}
+                onClick={onCancel}
+              >
+                Cancel
+              </Button>
+              <Button
+                bg="#4169E1"
+                color="white"
+                w="50%"
+                _hover={{
+                  bg: "#153289",
+                }}
+                type="submit"
+              >
+                Confirm
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Flex>
+    </DefaultLayout>
+  );
+};
+
+export default UserChangePassword;
