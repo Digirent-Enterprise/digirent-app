67e9862c19436526e5e4439162b5e0fd4bd62f70
minh0811 <vokhaiminh0811@gmail.com>
2022-08-28 12:08:04 +0700

    fix: Fix product page breaking and make it responsive

--- src/components/layouts/navigation/AdminTab/AdminTab.tsx
+++ src/components/layouts/navigation/AdminTab/AdminTab.tsx
@@ -4,31 +4,25 @@ const AdminTab = () => {
   return (
     <div className="px-4 py-6">
       <div className="flex justify-center ">
-        <ul className="flex">
+        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
           <li className="mr-6">
-            <a className="text-black hover:text-blue-100 link" href="/admin">
+            <a className="inline-block p-4 w-full bg-white" href="/admin">
               General
             </a>
           </li>
           <li className="mr-6">
-            <a
-              className="text-black hover:text-blue-100 link"
-              href="/admin/chat"
-            >
+            <a className="inline-block p-4 w-full bg-white" href="/admin/chat">
               Chat
             </a>
           </li>
           <li className="mr-6">
-            <a
-              className="text-black hover:text-blue-100 link"
-              href="/admin/users"
-            >
+            <a className="inline-block p-4 w-full bg-white" href="/admin/users">
               Users
             </a>
           </li>
           <li className="mr-6">
             <a
-              className="text-black hover:text-blue-100 link"
+              className="inline-block p-4 w-full bg-white"
               href="/admin/products"
             >
               Products
@@ -36,7 +30,7 @@ const AdminTab = () => {
           </li>
           <li className="mr-6">
             <a
-              className="text-black hover:text-blue-100 link"
+              className="inline-block p-4 w-full bg-white"
               href="/admin/transactions"
             >
               Transactions
@@ -44,7 +38,7 @@ const AdminTab = () => {
           </li>
           <li className="mr-6">
             <a
-              className="text-black hover:text-blue-100 link"
+              className="inline-block p-4 w-full bg-white"
               href="/admin/add-product"
             >
               Add Product
