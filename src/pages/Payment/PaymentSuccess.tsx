c9da1a69a61396db0d1fac0178f524a7535942d2
GiangNguyenn <giang.nguyen@skedulo.com>
2022-08-29 08:42:57 +0700

    Run format

--- src/pages/Payment/PaymentSuccess.tsx
+++ src/pages/Payment/PaymentSuccess.tsx
@@ -1,32 +1,34 @@
 import { useEffect, useState } from "react";
-import { customAxios } from "../../http-common";
 import qs from "qs";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
+import { customAxios } from "../../http-common";
 
 const PaymentSuccess = () => {
   const queryParams = new URLSearchParams(window.location.search);
   const payment_intent = queryParams.get("payment_intent");
   const payment_intent_client_secret = queryParams.get(
-    "payment_intent_client_secret"
+    "payment_intent_client_secret",
   );
   const [newTrans, setNewTrans] = useState({});
   const [intent, setIntent] = useState({});
 
   const _updateTransactionStatus = async () => {
-    await customAxios().put('transaction/update-transaction', qs.stringify({
-      status: 'paid',
-      intent: payment_intent_client_secret,
-    }))
-  }
+    await customAxios().put(
+      "transaction/update-transaction",
+      qs.stringify({
+        status: "paid",
+        intent: payment_intent_client_secret,
+      }),
+    );
+  };
 
   const _fetchTransaction = async () => {
-   return  await  customAxios().get('transaction/get-transaction-by-intent', {
+    return customAxios().get("transaction/get-transaction-by-intent", {
       params: {
-        intent: payment_intent_client_secret
-      }
-    })
-  }
-
+        intent: payment_intent_client_secret,
+      },
+    });
+  };
 
   const _fetchIntent = async () => {
     const response = await customAxios().get("payment-intent", {
@@ -34,11 +36,11 @@ const PaymentSuccess = () => {
     });
     if (response && response.data) {
       setIntent(response.data);
-      if (response.data.charges.data[0].paid){
-        await _updateTransactionStatus()
-        toast.success('You have paid your order.')
+      if (response.data.charges.data[0].paid) {
+        await _updateTransactionStatus();
+        toast.success("You have paid your order.");
         const newTransState = await _fetchTransaction();
-        if (newTransState && newTransState.data){
+        if (newTransState && newTransState.data) {
           setNewTrans(newTransState.data);
         }
       }
@@ -48,6 +50,6 @@ const PaymentSuccess = () => {
   useEffect(() => {
     _fetchIntent();
   }, []);
-  return newTrans ? <div> {JSON.stringify(newTrans)} </div>  :<> </>;
+  return newTrans ? <div> {JSON.stringify(newTrans)} </div> : <> </>;
 };
 export default PaymentSuccess;
