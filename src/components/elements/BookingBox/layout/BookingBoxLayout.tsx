b53721fc41c8240e4ab5d0842a244b0623af0179
bee <vogiabao292@gmail.com>
2022-08-30 22:58:08 +0700

    feat: admin update user

--- src/components/elements/BookingBox/layout/BookingBoxLayout.tsx
+++ src/components/elements/BookingBox/layout/BookingBoxLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { toast } from "react-toastify";
@@ -9,6 +9,8 @@ import "./Datepicker.css";
 const BookingBoxLayout = ({ productData }: any) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [mappedProductData, setMappedProductData] = useState<any>(productData);
+  const [excludedDates, setExcludedDates] = useState<any>([]);
   const onChange = (dates: any) => {
     const start = dates;
     setStartDate(start);
@@ -25,12 +27,36 @@ const BookingBoxLayout = ({ productData }: any) => {
     return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join("/");
   };
 
+  const _onHandleAddDates = () => {
+    const exclude = [...excludedDates];
+    for (let item of productData.excludeIntervals) {
+      if (item.start && item.end) {
+        let startTime = new Date(item.start);
+        const endTime = new Date(item.end);
+        exclude.push(startTime.setDate(startTime.getDate() - 1));
+        exclude.push(endTime);
+        exclude.push(startTime);
+        // @ts-ignore
+        while (startTime < endTime) {
+          startTime = new Date(startTime.setDate(startTime.getDate() + 1));
+          exclude.push(startTime);
+        }
+      }
+    }
+    setExcludedDates(exclude);
+  };
+
   if (startDate > endDate) {
     toast.warning("Please choose valid rental period", {
       theme: "dark",
     });
     setEndDate(startDate);
   }
+
+  useEffect(() => {
+    _onHandleAddDates();
+  }, []);
+
   return (
     <div className="flex flex-col justify-center w-full gap-2">
       <div className="flex flex-col justify-center mt-10 lg:flex-row">
@@ -46,17 +72,17 @@ const BookingBoxLayout = ({ productData }: any) => {
               <div className="flex gap-x-2 mt-5 flex-col md:flex-row lg:flex-row">
                 <DatePicker
                   wrapperClassName="date-picker"
-                  selected={startDate}
                   onChange={onChange}
                   minDate={new Date()}
                   inline
+                  excludeDates={excludedDates}
                 />
                 <DatePicker
                   wrapperClassName="date-picker"
-                  selected={endDate}
                   onChange={onChange2}
                   minDate={startDate}
                   inline
+                  excludeDates={excludedDates}
                 />
               </div>
             </div>
