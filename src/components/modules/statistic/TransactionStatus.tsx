18be006300b78679e5e361869b6f2ade23e72eb9
GiangNguyenn <giang.nguyen@skedulo.com>
2022-09-05 00:13:59 +0700

    fix: status chart not showing

--- src/components/modules/statistic/TransactionStatus.tsx
+++ src/components/modules/statistic/TransactionStatus.tsx
@@ -1,38 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { customAxios } from "../../../http-common";
 import PieChart from "./base/PieChart";
 
-const TransactionStatus = () => {
-  const [chartData, setChartData] = useState({}) as any;
+interface IChartData {
+  pending: number;
+  paid: number;
+}
 
-  const StatisticChart = () => {
+const TransactionStatus = () => {
+  const [chartData, setChartData] = useState<IChartData>({
+    pending: 0,
+    paid: 0,
+  });
+
+  useEffect(() => {
     customAxios()
       .get("statistic/transaction-status")
       .then((res) => {
         console.log("res", res.data);
-        setChartData({
-          labels: Object.keys(res.data),
-          datasets: [
-            {
-              data: Object.values(res.data),
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-              ],
-              borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
-              borderWidth: 1,
-            },
-          ],
-        });
+        console.log("Object.keys(res.data) :>> ", Object.keys(res.data));
+
+        setChartData(res.data);
+        
+        // console.log("chartData :>> ", chartData);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
-  useEffect(() => {
-    StatisticChart();
   }, []);
-  return <PieChart data={chartData} title="Transaction Status" />;
+
+  const preparedChartData = {
+    labels: chartData ? Object.keys(chartData) : [],
+    datasets: [
+      {
+        data: chartData ? Object.values(chartData) : [],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <>
+      {console.log("chartData :>> ", chartData)}
+      <PieChart data={preparedChartData} title="Transaction Status" />
+    </>
+  );
 };
 
 export default TransactionStatus;
